ffde36a26f7eebd97a0c1f07b325af4f16459840
Ashu-coder98 <31449780+Ashu-coder98@users.noreply.github.com>
2019-07-12 12:18:16 +0530

    schedule_limit_time set to 30 minutes
    functionality of lowest_schedule

--- redash/settings/dynamic_settings.py
+++ redash/settings/dynamic_settings.py
@@ -4,7 +4,7 @@ from .helpers import int_or_none
 
 # Replace this method with your own implementation in case you want to limit the time limit on certain queries or users.
 def query_time_limit(is_scheduled, user_id, org_id):
-    scheduled_time_limit = int_or_none(os.environ.get('REDASH_SCHEDULED_QUERY_TIME_LIMIT', None))
+    scheduled_time_limit = int_or_none(os.environ.get('REDASH_SCHEDULED_QUERY_TIME_LIMIT', 30))
     adhoc_time_limit = int_or_none(os.environ.get('REDASH_ADHOC_QUERY_TIME_LIMIT', None))
 
     return scheduled_time_limit if is_scheduled else adhoc_time_limit
