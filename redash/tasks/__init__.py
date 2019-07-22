f3e93389bb6ce5fbdad3a037d0b77965caeb12d5
Ashu-coder98 <31449780+Ashu-coder98@users.noreply.github.com>
2019-07-22 11:37:58 +0530

    Update __init__.py

--- redash/tasks/__init__.py
+++ redash/tasks/__init__.py
@@ -1,5 +1,5 @@
 from .general import record_event, version_check, send_mail, sync_user_details
-from .queries import (QueryTask, QueryTaskTracker, refresh_queries, refresh_schemas, cleanup_tasks, cleanup_query_results, execute_query,
+from .queries import (QueryTask, QueryTaskTracker, refresh_queries, refresh_schemas,cleanup_query_results, execute_query,
     check_expired_schedule_queries)
 from .alerts import check_alerts_for_query
 
